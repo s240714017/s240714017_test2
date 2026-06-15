cfbe1f837fbe53e3b41cf525d70d75b57b6a3cd8
s240714017 <s240714017@g.tohoku-gakuin.ac.jp>
2026-06-15 23:57:54 +0900

    Add files via upload

--- drawing.js
+++ drawing.js
@@ -7,23 +7,23 @@ let size=4;
 
 let rotDig; //講義資料外の変数宣言
 
-let color_var;///自作色変数
-let type_var;///自作図形変数
-let size_var;///大きさ指定変数
+/// 自作変数
+let color_var; //自作色変数
+let type_var; //自作図形変数
+let size_var; //大きさ指定変数
 
-let check1_var=false;///色変更の完了変数
-let check2_var=false;///図形変更の完了変数
-let check3_var=false;///大きさ変更の完了変数
+let check1_var=false; //色変更の完了変数
+let check2_var=false; //図形変更の完了変数
+let check3_var=false; //大きさ変更の完了変数
 
-let my_w_var;
-let my_h_var;
-let my_r_var;
-
-
+let my_w_var; //幅指定変数
+let my_h_var; //高さ指定変数
+let my_r_var; //半径指定変数
 
 
-/// 色関数
-function black_fnc(){
+
+/// 色関数,自作
+function black_fnc(){ //黒
     color_var=0;
     check1_var=true;
     draw_check_fnc();
@@ -31,7 +31,7 @@ function black_fnc(){
     console.log("check1_var="+check1_var);
     console.log();
 }
-function white_fnc(){
+function white_fnc(){ //白
     color_var=1;
     check1_var=true;
     draw_check_fnc();
@@ -39,7 +39,7 @@ function white_fnc(){
     console.log("check1_var="+check1_var);
     console.log();
 }
-function red_fnc(){
+function red_fnc(){ //赤
     color_var=2;
     check1_var=true;
     draw_check_fnc();
@@ -47,7 +47,7 @@ function red_fnc(){
     console.log("check1_var="+check1_var);
     console.log();
 }
-function orange_fnc(){
+function orange_fnc(){ //オレンジ
     color_var=3;
     check1_var=true;
     draw_check_fnc();
@@ -55,7 +55,7 @@ function orange_fnc(){
     console.log("check1_var="+check1_var);
     console.log();
 }
-function yellow_fnc(){
+function yellow_fnc(){ //黄
     color_var=4;
     check1_var=true;
     draw_check_fnc();
@@ -63,7 +63,7 @@ function yellow_fnc(){
     console.log("check1_var="+check1_var);
     console.log();
 }
-function green_fnc(){
+function green_fnc(){ //緑
     color_var=5;
     check1_var=true;
     draw_check_fnc();
@@ -71,7 +71,7 @@ function green_fnc(){
     console.log("check1_var="+check1_var);
     console.log();
 }
-function blue_fnc(){
+function blue_fnc(){ //青
     color_var=6;
     check1_var=true;
     draw_check_fnc();
@@ -79,7 +79,7 @@ function blue_fnc(){
     console.log("check1_var="+check1_var);
     console.log();
 }
-function navy_fnc(){
+function navy_fnc(){ //紺
     color_var=7;
     check1_var=true;
     draw_check_fnc();
@@ -87,7 +87,7 @@ function navy_fnc(){
     console.log("check1_var="+check1_var);
     console.log();
 }
-function purple_fnc(){
+function purple_fnc(){ //紫
     color_var=8;
     check1_var=true;
     draw_check_fnc();
@@ -97,8 +97,9 @@ function purple_fnc(){
 }
 
 
-/// 図形関数
-function Ccl_fnc(){
+
+/// 図形関数,自作
+function Ccl_fnc(){ //円
     type_var=0;
     check2_var=true;
     draw_check_fnc();
@@ -106,7 +107,7 @@ function Ccl_fnc(){
     console.log("check2_var="+check2_var);
     console.log();
 }
-function Line_fnc(){
+function Line_fnc(){ //直線
     type_var=1;
     check2_var=true;
     draw_check_fnc();
@@ -114,7 +115,7 @@ function Line_fnc(){
     console.log("check2_var="+check2_var);
     console.log();
 }
-function Rct_fnc(){
+function Rct_fnc(){ //長方形
     type_var=2;
     check2_var=true;
     draw_check_fnc();
@@ -122,7 +123,7 @@ function Rct_fnc(){
     console.log("check2_var="+check2_var);
     console.log();
 }
-function Tri_fnc(){
+function Tri_fnc(){ //三角形
     type_var=3;
     check2_var=true;
     draw_check_fnc();
@@ -130,7 +131,7 @@ function Tri_fnc(){
     console.log("check2_var="+check2_var);
     console.log();
 }
-function Squ_fnc(){
+function Squ_fnc(){ //正方形
     type_var=4;
     check2_var=true;
     draw_check_fnc();
@@ -138,7 +139,7 @@ function Squ_fnc(){
     console.log("check2_var="+check2_var);
     console.log();
 }
-function Ccl_H_fnc(){
+function Ccl_H_fnc(){ //半円
     type_var=5;
     check2_var=true;
     draw_check_fnc();
@@ -150,8 +151,8 @@ function Ccl_H_fnc(){
 
 
 
-/// 大きさ関数
-function small_fnc(){
+/// 大きさ関数,自作
+function small_fnc(){ //小さめ
     size_var=0;
     check3_var=true;
     draw_check_fnc();
@@ -159,7 +160,7 @@ function small_fnc(){
     console.log("check3_var="+check3_var);
     console.log();
 }
-function normal_fnc(){
+function normal_fnc(){ //普通
     size_var=1;
     check3_var=true;
     draw_check_fnc();
@@ -167,7 +168,7 @@ function normal_fnc(){
     console.log("check3_var="+check3_var);
     console.log();
 }
-function big_fnc(){
+function big_fnc(){ //大きめ
     size_var=2;
     check3_var=true;
     draw_check_fnc();
@@ -175,7 +176,7 @@ function big_fnc(){
     console.log("check3_var="+check3_var);
     console.log();
 }
-function my_size_fnc(){
+function my_size_fnc(){ //自分で指定
     size_var=3;
     check3_var=true;
     draw_check_fnc();
@@ -188,11 +189,14 @@ function my_size_fnc(){
 
 
 
+/// リセット関数,自作
 function reset_fnc(){
     oChange();
-    check1_var=false;///色変更の完了変数
-    check2_var=false;///図形変更の完了変数
-    check3_var=false;///大きさ変更の完了変数
+    check1_var=false;//色変更の初期化
+    check2_var=false;//図形変更の初期化
+    check3_var=false;//大きさ変更の初期化
+
+    //メッセージ用idの初期化(計8つ)
     document.getElementById("mess_id").innerHTML="";
     document.getElementById("my_w_id").innerHTML="";
     document.getElementById("my_h_id").innerHTML=""
@@ -200,28 +204,31 @@ function reset_fnc(){
     document.getElementById("my_w_mess_id").innerHTML="";
     document.getElementById("my_h_mess_id").innerHTML=""
     document.getElementById("my_r_mess_id").innerHTML=""
+    
+    //w(幅),h(高さ),r(半径)の初期化
     my_w_var=0;
     my_h_var=0;
     my_r_var=0;
 }
 
+/// w,h,rの入力補助関数,自作
 function my_size_sup_fnc(){
+    //値の入力位置の補助を行う
     document.getElementById("my_w_mess_id").innerHTML="wの値=";
     document.getElementById("my_h_mess_id").innerHTML="hの値=";
     document.getElementById("my_r_mess_id").innerHTML="rの値=";
 }
 
-
+/// 絵を描けるかどうか確認する関数,自作
 function draw_check_fnc(){
-    if(check1_var==true && check2_var==true && check3_var==true){
+    if(check1_var==true && check2_var==true && check3_var==true){ //三項目のボタンが入力されていた時
         document.getElementById("mess_id").innerHTML="絵の書いてみよう";
-    } else {
+    } else { //いずれかのボタンが未入力時
         document.getElementById("mess_id").innerHTML="「色」,「図形」,「大きさ」の三つのボタンを入力してください";
     }
 }
 
-
-
+/// ok関数,大きさ変更の反映を行う,自作
 function ok_fnc(){
     if(size_var==3){
         my_w_var=document.getElementById("my_w_id").value;
@@ -234,15 +241,14 @@ function ok_fnc(){
     }
 }
 
-
-
-
+// 通常時の処理関数,講義資料
 function init(){
     canvas.addEventListener("touchstart", touchStart, false);
     canvas.addEventListener("touchmove", touchMove, false);
     window.addEventListener("orientationchange", oChange, true);
 }
 
+// 端末の傾きに応じた絵のリセット関数,講義資料
 function oChange()
 {
     var flag=confirm("絵を消去しますか？");
@@ -253,6 +259,7 @@ function oChange()
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+// 端末のタッチに反応する関数,講義資料
 function touchStart(event)
 {
     if(event.touches.length>1){
@@ -262,12 +269,14 @@ function touchStart(event)
     old_y=event.touches[0].pageY;
 }
 
+// 動きに反応する関数,講義資料+一部変更
 function touchMove(event){
     let c_x;
     let c_y;
     event.preventDefault();
     c_x=event.touches[0].pageX;
     c_y=event.touches[0].pageY;
+    //if,else ifでボタンに応じた処理の分岐(=大きさ,図形,値の指示の有無の変更)
     if(check1_var==true && check2_var==true && check3_var==true){
         if(type_var==0){
             if(size_var==0){
@@ -333,9 +342,10 @@ function touchMove(event){
     old_y=c_y;
 }
 
+// 直線関数,講義資料+一部変更
 function drawLine(x1, y1, x2, y2, psize, color_v)///引数変更
 {
-    ///以下のif処理,else if処理の追加
+    ///以下のif処理,else if処理の追加,色への対応
     if(color_v==0){
         ctx.strokeStyle="black";
     } else if(color_v==1){
@@ -363,10 +373,11 @@ function drawLine(x1, y1, x2, y2, psize, color_v)///引数変更
     ctx.stroke();
 }
 
+// 長方形関数,講義資料+一部変更
 function drawRct(x, y, w, h, color_v, isfill)
 {
     if(isfill==true){
-        ///以下のif処理,else if処理の追加
+        ///以下のif処理,else if処理の追加,色への対応
         if(color_v==0){
             ctx.fillStyle="black";
         } else if(color_v==1){
@@ -388,7 +399,7 @@ function drawRct(x, y, w, h, color_v, isfill)
         }
         ctx.fillRect(x, y, w, h);
     } else if(isfill==false) {
-        ///以下のif処理,else if処理の追加
+        ///以下のif処理,else if処理の追加,色への対応
         if(color_v==0){
             ctx.strokeStyle="black";
         } else if(color_v==1){
@@ -412,9 +423,10 @@ function drawRct(x, y, w, h, color_v, isfill)
     }
 }
 
+// 三角形関数,講義資料+一部変更
 function drawTri(x, y, w, h, color_v)
 {
-    ///以下のif処理,else if処理の追加
+    ///以下のif処理,else if処理の追加,色への対応
     if(color_v==0){
         ctx.fillStyle="black";
     } else if(color_v==1){
@@ -442,9 +454,10 @@ function drawTri(x, y, w, h, color_v)
     ctx.fill();
 }
 
+// 円関数,講義資料+一部変更
 function drawCcl(x, y, r, color_v)///引数変更
 {
-    ///以下のif処理,else if処理の追加
+    ///以下のif処理,else if処理の追加,色への対応
     if(color_v==0){
         ctx.fillStyle="black";
     } else if(color_v==1){
@@ -470,27 +483,12 @@ function drawCcl(x, y, r, color_v)///引数変更
     ctx.fill();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//自作図形
+/// 自作図形関数
+/// 正方形関数,講義資料+一部変更
 function drawSqu(x, y, w, h, color_v, isfill)
 {
     if(isfill==true){
-        ///以下のif処理,else if処理の追加
+        ///以下のif処理,else if処理の追加,色への対応
         if(color_v==0){
             ctx.fillStyle="black";
         } else if(color_v==1){
@@ -512,7 +510,7 @@ function drawSqu(x, y, w, h, color_v, isfill)
         }
         ctx.fillRect(x, y, w, h);
     } else if(isfill==false) {
-        ///以下のif処理,else if処理の追加
+        ///以下のif処理,else if処理の追加,色への対応
         if(color_v==0){
             ctx.strokeStyle="black";
         } else if(color_v==1){
@@ -536,9 +534,10 @@ function drawSqu(x, y, w, h, color_v, isfill)
     }
 }
 
+// 半円関数,講義資料+一部変更
 function drawCcl_H(x, y, r, color_v)///引数変更
 {
-    ///以下のif処理,else if処理の追加
+    ///以下のif処理,else if処理の追加,色への対応
     if(color_v==0){
         ctx.fillStyle="black";
     } else if(color_v==1){
